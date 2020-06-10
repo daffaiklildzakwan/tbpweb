82f0fe6e103daa4c3b6c360cfa4eb50a6770c87b
muhammadrafi1018 <63406260+muhammadrafi1018@users.noreply.github.com>
2020-06-10 19:27:47 +0700

    View : show.blade.php dan index.blade.php

---
+++ resources/views/K03/myinterns/show.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.admin')
+
+@section('breadcrumb')
+    {!! cui()->breadcrumb([
+        'Home' => route('home'),
+        'Internship' => route('frontend.myinterns.index'),
+        'Detail' => '#'
+    ]) !!}
+@endsection
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+
+                {{-- CARD HEADER--}}
+                <div class="card-header">
+                    <strong><i class="cil-list"></i> Detail KP</strong>
+                </div>
+
+                {{-- CARD BODY--}}
+                <div class="card-body">
+                    <table cellpadding='10'>
+                    @foreach($internships as $internship)
+                        <tr>
+                            <td>Judul KP</td><td>: {{$internship->title}}</td>
+                        </tr>   
+                        <tr>
+                            <td>Dosen Pembimbing KP Lapangan</td><td>: {{$internship->field_advisor_name}}</td>
+                        </tr>   
+                        <tr>
+                            <td>Tempat Pelaksanaan KP</td>
+                        </tr> 
+                        <tr>
+                            <td>- Nama Instansi / Perusahaan</td><td>: {{$internship->proposal->agency->name}}</td>
+                        </tr>   
+                        <tr>
+                            <td>- Alamat Instansi / Perusahaan</td><td>: {{$internship->proposal->agency->address}}</td>
+                        </tr>   
+                        <tr>
+                            <td>Waktu Pelaksanaan KP</td><td>: {{$internship->start_at}} s/d {{$internship->end_at}}</td>
+                        </tr> 
+                    @endforeach
+                    </table>
+                </div><!--card-body-->
+
+                {{--CARD FOOTER--}}
+                <div class="card-footer">
+                        {!! cui()->btn(route('frontend.myinterns.edit', [$myinterns]),'cil-folder',' Upload Berkas') !!}    
+                </div>
+            </div><!--card-->
+        </div><!--col-->
+    </div><!--row-->
+
+@endsection
